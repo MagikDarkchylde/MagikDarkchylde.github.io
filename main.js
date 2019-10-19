0506360ed7dcabe295a4e42a3e4dbebd393acd5b
MagikDarkchylde <natashac.romanoff@gmail.com>
2019-10-19 10:56:44 -0300

    added player two

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { game } from "./engine/engine.js";
 
-let e = game(480, 240, init, ["img/unnamed.png"]);
+let e = game(480, 240, init, ["img/unnamed.png", "img/gabe.png"]);
 e.start();
 e.canvas.style.background = "black";
 e.scaleToWindow();
@@ -9,11 +9,22 @@ window.addEventListener("resize", event => {
 });
 
 function init() {
-	console.log ("Starting");
+	//Creando Player 1
 	let frames = e.filmstrip(e.assets["img/unnamed.png"], 24, 24);
 	let player = e.sprite(frames);
+	player.x = 12;
+	player.y = 36;
 	player.fps = 12;
 	player.playSequence([1,6]);
+	// player.show(0);
+
+	//creando player 2
+	let frames2 = e.filmstrip(e.assets["img/gabe.png"], 24, 24);
+	let player2 = e.sprite(frames2);
+	player2.x = 60;
+	player2.y = 36;
+	player2.fps = 12;
+	player2.playSequence([1,6]);
 	//change state to loop
 	e.state = play;
 }
