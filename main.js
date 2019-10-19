90668986b30ee05570b6ed7c7d8c3e45dc1d95b6
MagikDarkchylde <natashac.romanoff@gmail.com>
2019-10-19 10:34:20 -0300

    add fps and play sequence

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ import { game } from "./engine/engine.js";
 
 let e = game(480, 240, init, ["img/unnamed.png"]);
 e.start();
+e.canvas.style.background = "black";
 
 function init() {
 	console.log ("Starting");
 	let frames = e.filmstrip(e.assets["img/unnamed.png"], 24, 24);
 	let player = e.sprite(frames);
+	player.fps = 12;
+	player.playSequence([1,6]);
 }
 
