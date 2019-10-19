072224dc0d268ee71e086951bdbb56b6d9b29981
MagikDarkchylde <natashac.romanoff@gmail.com>
2019-10-19 10:45:22 -0300

    resize screen

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let e = game(480, 240, init, ["img/unnamed.png"]);
 e.start();
 e.canvas.style.background = "black";
 e.scaleToWindow();
+window.addEventListener("resize", event => {
+	e.scaleToWindow();
+});
 
 function init() {
 	console.log ("Starting");
@@ -11,5 +14,10 @@ function init() {
 	let player = e.sprite(frames);
 	player.fps = 12;
 	player.playSequence([1,6]);
+	//change state to loop
+	e.state = play;
 }
 
+function play() {
+	console.log("this will be called 60 frames per second");
+}
