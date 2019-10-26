466b40310da6fcdce5f5fd8c85c030dbd2508a39
MagikDarkchylde <natashac.romanoff@gmail.com>
2019-10-26 09:57:45 -0300

    added Atlas

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 import { game } from "./engine/engine.js";
 
-let e = game(480, 240, init, ["img/unnamed.png", "img/gabe.png"]);
+let e = game(
+	480, 
+	240, 
+	init, 
+	[
+		"img/unnamed.png", 
+		"img/gabe.png",
+		"img/atlas.png"
+	]);
 e.start();
 e.canvas.style.background = "#2e86de";
 e.scaleToWindow();
 window.addEventListener("resize", event => {
 	e.scaleToWindow();
 });
+//Global Vars
+let playerSprite, player;
 
 function init() {
 	//Creando Player 1
 	let frames = e.filmstrip(e.assets["img/unnamed.png"], 24, 24);
-	let player = e.sprite(frames);
-	player.x = 12;
-	player.y = 36;
-	player.fps = 12;
-	player.playSequence([1,6]);
-	// player.show(0);
+	let playerSprite = e.sprite(frames);
+	playerSprite.x = 12;
+	playerSprite.y = 36;
+	playerSprite.fps = 12;
+	playerSprite.playSequence([1,6]);
+	//Player Shadow
+	player = e.rectangle(10, 5, "white", "none", 0, 50, 50);
+	player.add(playerSprite);
 
-	//creando player 2
-	let frames2 = e.filmstrip(e.assets["img/gabe.png"], 24, 24);
-	let player2 = e.sprite(frames2);
-	player2.x = 60;
-	player2.y = 36;
-	player2.fps = 12;
-	player2.playSequence([1,6]);
+
 	//change state to loop
 	e.state = play;
 }
